693e7e4125954a6d380bfa7ef9e8a1a6947f0829
Enrique <antonio.garcia.gallur@gmail.com>
2019-01-20 20:49:41 +0100

    Signed-off-by: Enrique <antonio.garcia.gallur@gmail.com>

--- src/main/java/es/salesianos/edu/spring/Coche.java
+++ src/main/java/es/salesianos/edu/spring/Coche.java
@@ -1,22 +1,65 @@
 package es.salesianos.edu.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Coche {
 	
-	static Motor motor;
+	@Autowired
+	private Motor motor;
+	@Autowired
+	@Qualifier("lucesCortas")
+	private Luces lucesCortas;
+	@Autowired
+	@Qualifier("lucesLargas")
+	private Luces lucesLargas;
+	@Autowired
+	private Frenos frenos;
 	
 	public static void main(String[] args) {
+		new Coche();
+	}
+	
+	public Coche() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-context.xml");
-	    motor = (Motor) context.getBean("motor");
-	    Frenos frenos = (Frenos) context.getBean("frenos");
-	    Luces lucesCortas = (Luces) context.getBean("lucesCortas");
-	    Luces lucesLargas = (Luces) context.getBean("lucesLargas");
-	    motor.arrancar();
-	    frenos.braking();
-	    System.out.println(lucesCortas.getIluminacion());
-	    System.out.println(lucesLargas.getIluminacion());
+	    AutowireCapableBeanFactory acbFactory = context.getAutowireCapableBeanFactory();
+		acbFactory.autowireBean(this);
+	    getMotor().arrancar();
+	    getFrenos().braking();
+	    System.out.println(getLucesCortas().getIluminacion());
+	    System.out.println(getLucesLargas().getIluminacion());
 	    
+	}
+	
+	public Motor getMotor() {
+		return motor;
+	}
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+	
+	public Luces getLucesCortas() {
+		return lucesCortas;
+	}
+	
+	public void setLucesCortas(Luces lucesCortas) {
+		this.lucesCortas = lucesCortas;
+	}
+	
+	public void setLucesLargas(Luces lucesLargas) {
+		this.lucesLargas = lucesLargas;
+	}
+	
+	public Luces getLucesLargas() {
+		return lucesLargas;
+	}
+	public Frenos getFrenos() {
+		return frenos;
+	}
+	public void setFrenos(Frenos frenos) {
+		this.frenos = frenos;
 	}
 }
